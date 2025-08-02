1493a14725726586cc8f0bb172050a263f471ff3
ohmymoon-216 <ohmymoon@gmail.com>
2025-08-02 12:08:48 +0900

    study2

--- src/main/java/kr/ohmymoon/study/spring6study/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/kr/ohmymoon/study/spring6study/hellospring/exrate/WebApiExRateProvider.java
@@ -4,15 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import kr.ohmymoon.study.spring6study.hellospring.api.SimpleApiExecutor;
 import kr.ohmymoon.study.spring6study.hellospring.payment.ExRateProvider;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Map;
-import java.util.stream.Collectors;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class WebApiExRateProvider implements ExRateProvider {
